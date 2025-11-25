661618cb674450a554a82c8d757591bb0a7202e9
cawaa <sasaokulova055@gmail.com>
2025-11-25 08:29:28 +0500

    Create FunsMeow.java

---
+++ FunsMeow.java
@@ -0,0 +1,11 @@
+public class FunsMeow {
+
+    //Метод принимает объект и вызывает его
+    public static void meowsCare(Meowable m) {
+
+        for (int i = 0; i < 5; i++) {
+            m.meow();
+        }
+
+    }
+}
