8a3fcd3df1000652bc4df86ff318a0c8aaed898c
cawaa <sasaokulova055@gmail.com>
2025-11-25 08:20:45 +0500

    Create FractionOperations.java

---
+++ FractionOperations.java
@@ -0,0 +1,5 @@
+public interface FractionOperations {
+    double getDoubleValue(); // возвращает вещественное значение
+    void setNumerator(int numerator); //числитель
+    void setDenominator(int denominator); //знаменатель
+}
