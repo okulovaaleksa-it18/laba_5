df71cb93eb317576a779b2664a5feb956993be31
cawaa <sasaokulova055@gmail.com>
2025-12-01 22:49:07 +0500

    Create Counter.java

---
+++ Counter.java
@@ -0,0 +1,14 @@
+package ru.okulova.lab5.cats;
+
+public class Counter {
+    private int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
